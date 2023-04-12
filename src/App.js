d4a77ac3d16be97cc21f7d2c1778a12e8dc4cfb9
Abfahim21 <abrarfahim21@gmail.com>
2023-04-13 01:15:47 +0600

    rcc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hello from './components/Hello';
+import Hi from './components/Hi';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Hi/>
     </div>
   );
 }
