453b669b30f817d243af3200151eb987995e8bf5
Abfahim21 <abrarfahim21@gmail.com>
2023-04-28 03:09:06 +0600

    react setState

--- src/components/PropsFunctional.js
+++ src/components/PropsFunctional.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function PropsFunctional(props) {
+  const {name, heroName} = props
   return (
     <div>
-        <h2>Hello {props.name} a.k.a {props.heroName}.</h2>
+        <h2>Hello {name} a.k.a {heroName}.</h2>
     </div>
   )
 }
