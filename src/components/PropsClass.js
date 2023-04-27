33e70c3c89b81340d4c6863b81f6776bb9f2feb0
Abfahim21 <abrarfahim21@gmail.com>
2023-04-28 03:10:19 +0600

    react setState

--- src/components/PropsClass.js
+++ src/components/PropsClass.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 export default class PropsClass extends Component {
   render() {
+    const {name, heroName} = this.props
     return (
       <div>
-        <h2>Hello {this.props.name} a.k.a {this.props.heroName}.</h2>
+        <h2>Hello {name} a.k.a {heroName}.</h2>
       </div>
     )
   }
