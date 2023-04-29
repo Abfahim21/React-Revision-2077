97f15789f72c891efc862b9a8eb6138b36f640e4
Abfahim21 <abrarfahim21@gmail.com>
2023-04-29 23:49:27 +0600

    updating

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import State from './components/State';
 function App() {
   return (
     <div className="App">
-      <Hello/>
-      <Hi/>
-      <PropsFunctional name = "Abrar" heroName  = "Super Man"/>
-      <PropsClass name = "Nadia" heroName = "Wonder Woman"/>
-      <State/>
-      <Counter/>
+      <Hello />
+      <Hi />
+      <PropsFunctional name="Abrar" heroName="Super Man" />
+      <PropsClass name="Nadia" heroName="Wonder Woman of Bangladesh" />
+      <State />
+      <Counter />
     </div>
   );
 }
