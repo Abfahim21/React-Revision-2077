240de624272b052fbe923f82e31f0d17226c2dae
Abfahim21 <abrarfahim21@gmail.com>
2023-05-12 11:31:35 +0600

    List rendering

---
+++ src/components/PersonList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Person from './Person'
+
+export default function PersonList() {
+    const persons = [
+        {
+            id: 1,
+            name: 'Abrar Fahim',
+            age: 28,
+            skill: 'React.JS'
+        },
+        {
+            id: 2,
+            name: 'Farhan Sarkar',
+            age: 28,
+            skill: 'Vue.JS'
+        },
+        {
+            id: 3,
+            name: 'Nadia Sarkar',
+            age: 28,
+            skill: 'Angular.JS'
+        }
+    ]
+    const PersonList = persons.map(person =><Person person ={person}/>)
+  return (
+    <div>
+        {PersonList}
+    </div>
+  )
+}
