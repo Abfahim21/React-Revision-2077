1bca2923afe798248b7589975cbdf7bc8504862b
Abfahim21 <abrarfahim21@gmail.com>
2023-05-18 01:55:28 +0600

    form handling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       userName : ''
+    }
+  }
   render() {
     return (
       <div>
         <form action="">
           <label>Enter Username: </label>
-          <input type="text" />
+          <input type="text" value={this.state.userName}/>
         </form>
       </div>
     )
