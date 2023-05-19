4482426214e017791cf66ff3746c482df58e39ef
Abfahim21 <abrarfahim21@gmail.com>
2023-05-20 02:33:41 +0600

    forminggg

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ class Form extends Component {
   
     this.state = {
        userName : '',
-       comment : '',
-       select : 'react.js',
+       comments : '',
+       topic : "React.JS"
     }
   }
   handleUserNameChange = (event) =>{
@@ -25,12 +25,13 @@ class Form extends Component {
       topic: event.target.value
     })
   }
-  handleSubmit = event =>{
-    alert(`${this.state.userName} ${this.state.comment} ${this.state.topic}`)
+  handleSubmit = (event) =>{
+    alert(`${this.state.userName} ${this.state.comments} ${this.state.topic}`)
+    event.preventDefault()
   }
   render() {
     return (
-      <div onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <form action="">
             <label>Enter Username: </label>
@@ -39,19 +40,19 @@ class Form extends Component {
         </div>
         <div>
           <label>Comment: </label>
-          <textarea value={this.state.comment} onChange={this.state.handleCommentChange}></textarea>
+          <textarea value={this.state.comments} onChange={this.state.handleCommentChange}></textarea>
         </div>
         <div>
           <label>Select: </label>
-            <select value={this.state.select} onChange={this.handleTopicChange}>
-            <option>React.js</option>
-            <option>Laravel</option>
-            <option>Angular.Js</option>
-            <option>Next.JS</option>
+            <select value={this.state.topic} onChange={this.handleTopicChange}>
+            <option value="React.JS">React.js</option>
+            <option value="Laravel">Laravel</option>
+            <option value="Angular.JS">Angular.Js</option>
+            <option value="Next.JS">Next.JS</option>
             </select>
         </div>
-        <button>Submit</button>
-      </div>
+        <button type='submit'>Submit</button>
+      </form>
     )
   }
 }
