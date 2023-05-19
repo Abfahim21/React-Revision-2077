d6b6df2852b9a50c5e259cdfdf81825757304893
Abfahim21 <abrarfahim21@gmail.com>
2023-05-20 03:00:17 +0600

    fragments

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Person({person}) {
   return (
-    <div>
-        <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h2>
-    </div>
+    <React.Fragment>
+      <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h2>
+    </React.Fragment>
   )
 }
 
