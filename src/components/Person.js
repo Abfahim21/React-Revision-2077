2ce53df7f875a47bead59aae187c56030be0a3d2
Abfahim21 <abrarfahim21@gmail.com>
2023-05-24 03:21:53 +0600

    adding

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Person({person}) {
+  const {name, age, skill} = person
   return (
-    <React.Fragment>
-      <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h2>
-    </React.Fragment>
+    <div>
+       <h2>I am {name}. I am {age} years old. I know {skill}.</h2>
+    </div>
   )
 }
 
